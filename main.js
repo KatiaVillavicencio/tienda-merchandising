a04486c2a49a02318cf7ae2c3e8c2c17653b31ed
Katia Villavicencio <125634854+KatiaVillavicencio@users.noreply.github.com>
2023-05-24 00:51:19 +0200

    ajustesok

--- main.js
+++ main.js
@@ -2,6 +2,15 @@
 //tienda de merchandising//
 
 // 1er paso. Declaro funciones y constantes/variables globales, que se usaran para todas las funciones //
+// Constantes globales - Array de Productos
+
+const productos = [
+    new Producto(1, "polera", 120, 10, true),
+    new Producto(2, "tomatodo", 80, 15, false),
+    new Producto(3, "taza", 40, 30, true),
+    new Producto(4, "gorra", 50, 20, false),
+    new Producto(5, "llavero", 10, 100, true)
+];
 
 //Funciones://
 
@@ -21,75 +30,116 @@ function saludo() {
 
 //funcion para los productos//
 
-function Producto(nombre, precio, stock) {
+function Producto(id, nombre, precio, stock, descuento) {
+    this.id = id
     this.nombre = nombre
     this.precio = precio
     this.stock = stock
-
-    this.imprimirAnuncio = function () {
-        console.log(`Aprovecha el 20% de descuento en tu ${this.nombre}, disponible ${this.stock} unidades`);
-        return this.precio;
-    }
+    this.descuento = descuento
 
 };
 
-//funcion para elegir un producto//
+// funcion para imprimir anuncio
+/*function imprimirAnuncio() {
+    console.log(`Aprovecha el 20% de descuento en tu ${this.nombre}, disponible ${this.stock} unidades`);
+    return this.precio;
+}*/
 
+
+//funcion para mostrar y elegir un producto x ID//
+
+function mostrarProducto() {
+
+    let seleccion = prompt("Escriba el numero del producto deseado: 1. Polera 2. Tomatodo 3. Taza 4. Gorra 5. Llavero");
+
+    let productoBusqueda = productos.find(i => i.id == seleccion);
+
+    if (productoBusqueda) {
+        console.log("Producto si existe", productoBusqueda);
+        agregarProducto(productoBusqueda);
+
+    } else {
+        console.log("Producto no existe");
+        alert("Producto no existe");
+    }
+};
+
+// funcion para agregar productos
 let productosElegidos = [];
-
-function agregarProducto(nombre) {
-    productosElegidos.push(nombre);
+function agregarProducto(seleccion) {
+    productosElegidos.push(seleccion);
 }
 
-// funcion para sumar el total - el 20% de dscto.//
+// funcion para preguntar por mas productos
+function masProductos() {
+    const confirmar = confirm("Desea agregar mas productos?");
+    return confirmar;
+}
+
+// funcion para sumar el total incluyendo el 20% de dscto. en los productos indicados //
 
 function calcularTotal() {
     let total = 0;
     const descuento = 0.20;
 
     for (let i = 0; i < productosElegidos.length; i++) {
-        const precioConDescuentoFinal = productosElegidos[i].precio - (productosElegidos[i].precio * descuento);
-        console.log(precioConDescuentoFinal, "Precio con descuento final")
-        total += precioConDescuentoFinal;
+        if (productosElegidos[i].descuento) {
+            const precioConDescuentoFinal = productosElegidos[i].precio - (productosElegidos[i].precio * descuento);
+            total += precioConDescuentoFinal;
+        } else {
+            const precioSinDescuentoFinal = productosElegidos[i].precio;
+            console.log(precioSinDescuentoFinal, "Precio con descuento final")
+            total += precioSinDescuentoFinal;
+        }
     }
+
     total = console.log(`Gracias por su compra, precio total con dscto.: S/. ${total} soles`);
     return total;
 }
 
 
-// Constantes globales
-
-const producto1 = new Producto("polera", 120, 10)
-const producto2 = new Producto("tomatodo", 80, 15)
-const producto3 = new Producto("taza", 40, 30)
-const producto4 = new Producto("gorra", 50, 20)
-const producto5 = new Producto("llavero", 10, 100);
-
 
 /* 2do paso. Llamo a las funciones:
 1. Saludo
-2. Agregar productos
-3. Total
+2. Mostrar lista
+3. Elegir productos x ID
+4. Comprobar existencia del producto
+5. Agregar productos
+6. Consultar si desea agregar mas productos
+7. Filtrar producto por descuento
+8. Total
 */
 
 let mensajeSaludo = saludo();
 console.log("Mensaje de Saludo:", mensajeSaludo);
 
+mostrarProducto();
+
+while (masProductos()) {
+    mostrarProducto();
+};
+
+
+console.log(productosElegidos);
+
+calcularTotal();
+
+
+
+
 //respuestas por consola funcion imprimir anuncio//
 
-console.log("S/.",producto1.imprimirAnuncio());
-console.log("S/.",producto2.imprimirAnuncio(),);
-console.log("S/.",producto3.imprimirAnuncio(),);
+/* console.log("S/.", producto1.imprimirAnuncio());
+console.log("S/.", producto2.imprimirAnuncio(),);
+console.log("S/.", producto3.imprimirAnuncio(),); */
 
 
 //agregar productos por ejem: agregarProducto(producto3) y luego revisar: console.log(productosElegidos)
 
-agregarProducto(producto3), agregarProducto(producto5)
-console.log("productos elegidos", productosElegidos)
+/* agregarProducto(producto3), agregarProducto(producto5)
+console.log("productos elegidos", productosElegidos) */
 
 
-//total de la compra con descuento/
-let finalizar = calcularTotal();
 
 
 
